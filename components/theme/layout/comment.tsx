0936c9263e0ee79a6cbbbfe65450cc8d5e16e56f
Steve P <stevephuc.dev@gmail.com>
2024-07-23 15:52:55 +0300

    Giscus fix repo repoId

--- components/theme/layout/comment.tsx
+++ components/theme/layout/comment.tsx
@@ -10,7 +10,7 @@ export default function Comment() {
       <Giscus
         id="comments"
         repo="alteropen-com/alteropen"
-        repoId="R_kgDOMVlEcA="
+        repoId="R_kgDOMVlEcA"
         category="Announcements"
         categoryId="DIC_kwDOMVlEcM4CgxnM"
         mapping="pathname"
