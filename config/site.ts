82a3d9e7a264b6df06044f46a7af7f3857f3ac58
Steve P <stevephuc.dev@gmail.com>
2024-07-26 21:30:18 +0300

    fix github link

--- config/site.ts
+++ config/site.ts
@@ -7,7 +7,7 @@ export const siteConfig = {
     "https://p140.p3.n0.cdn.zight.com/items/mXuWl2pn/05d47b6c-c028-43f8-a368-18a35d132b9c.jpg",
   links: {
     twitter: "https://twitter.com/",
-    github: "https://git.new/alternative",
+    github: "https://github.com/alteropen-com/alteropen",
   },
   author: "Steven Alter",
   ref: "AlterOpen.com",
