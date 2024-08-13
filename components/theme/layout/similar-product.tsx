b352d234ff64ea15537b409a48e9737c802e451c
Steve P <stevephuc.dev@gmail.com>
2024-08-13 21:21:00 +0300

    StickyNav

--- components/theme/layout/similar-product.tsx
+++ components/theme/layout/similar-product.tsx
@@ -52,7 +52,7 @@ export default function SimilarProduct({ post }: { post: Alternative }) {
 
   return (
     <>
-      <h2 className="text-xl font-bold my-2" id="alternativeTo">
+      <h2 className="text-xl font-bold my-2" id="similar">
         {`${post.name.toUpperCase()} similar to`}
       </h2>
       <div className="grid gap-3 lg:gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
