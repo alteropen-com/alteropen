8f51ccf8fd5ee4813781818bc093244d3da01c58
Steve P <stevephuc.dev@gmail.com>
2024-09-16 07:44:17 +0300

    improve sumbit package

--- app/submit/page.tsx
+++ app/submit/page.tsx
@@ -7,13 +7,24 @@ export default function SubmitPage() {
   return (
     <div className="container mx-auto px-4 py-8 max-w-2xl">
       <h1 className="text-4xl font-bold text-center mt-10 mb-2">Submit Tool</h1>
-      <p className="text-xl text-center text-gray-600">
-        Submit Your Alternative Tool, Get Recommended
+      <p className="text-xl text-center text-gray-600 mt-5">
+        Submit Your Tool, Get Recommended
       </p>
       <div className="flex justify-center items-center">
-        <Badge>DR:39</Badge>
+        <Badge className="scale-[125%] mt-2">DR:39</Badge>
       </div>
-      <div className="mt-10 text-center">
+      <div className="mt-8 text-center">
+        <p className="mt-2 text-xl">
+          Try the{" "}
+          <Button variant="link" className="p-0 text-xl " asChild>
+            <Link href="/submit/pack" className="no-underline">
+              Submit Pack
+            </Link>
+          </Button>
+        </p>
+        <p className="mt-5 my-2">Or</p>
+      </div>
+      <div className="mt-8 text-center">
         <p className="mb-4 text-lg">Add the code to your homepage.</p>
         <code className="bg-foreground/10 p-2 rounded-md block mb-4">
           {
@@ -27,15 +38,6 @@ export default function SubmitPage() {
         <div className="min-h-[450px]">
           <SubmitVbout />
         </div>
-        <p className="my-2">Or</p>
-        <p className="mt-2 text-xl">
-          Try the{" "}
-          <Button variant="link" className="p-0 text-xl " asChild>
-            <Link href="/submit/pack" className="no-underline">
-              Submit Pack
-            </Link>
-          </Button>
-        </p>
       </div>
     </div>
   )
